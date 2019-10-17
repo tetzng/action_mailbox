54889228fb6317d291408100bf1ebfae93f4fabf
tetzng <ffffffffffffunk@gmail.com>
2019-10-17 20:32:35 +0900

    Update discussions_controller.rb

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -69,6 +69,6 @@ class DiscussionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def discussion_params
-      params.require(:discussion).permit(:title, :, :content, :)
+      params.require(:discussion).permit(:title, :content)
     end
 end
